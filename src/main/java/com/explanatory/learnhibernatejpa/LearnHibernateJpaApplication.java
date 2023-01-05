45dbed5a9475207a529a1091c679129c9dbd7561
Ajay Tidke <ajaytidke28297@gmail.com>
2023-01-05 18:32:43 +0530

    INital setup for Spring jdbc and jpa

---
+++ src/main/java/com/explanatory/learnhibernatejpa/LearnHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package com.explanatory.learnhibernatejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHibernateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/explanatory/learnhibernatejpa/LearnHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explanatory.learnhibernatejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
